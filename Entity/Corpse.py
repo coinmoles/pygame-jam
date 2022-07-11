375bb57e3c347eab379e1a7696a2df7ddda3f5b9
minjun0459 <minjun0459@naver.com>
2022-07-12 00:54:03 +0900

    added corpse

--- Entity/Corpse.py
+++ Entity/Corpse.py
@@ -1,4 +1,5 @@
-from Platform.Platform import Platform
+from Entity.Platform import Platform
 
 class Corpse(Platform):
-    def __init__(self):
+    def __init__(self, size, center):
+        super().__init__(size, (30,30,30), center)
--- Scene/GameScene.py
+++ Scene/GameScene.py
@@ -3,6 +3,7 @@ from Scene.Scene import Scene
 from Entity.Player import Player
 from Entity.Platform import Platform
 from Entity.Item import Item
+from constants import SCREEN
 
 
 class GameScene(Scene):
@@ -12,7 +13,7 @@ class GameScene(Scene):
         self.items = pg.sprite.Group()
 
         self.player = Player(self)
-        platform = Platform()
+        platform = Platform((SCREEN.width, 20), (255, 0, 0), (SCREEN.width / 2, SCREEN.height - 10))
         item = Item((50, 50), (0, 0, 255), (50, 50))
 
         self.platforms.add(platform)
@@ -26,6 +27,10 @@ class GameScene(Scene):
         self.player.move()
         for entity in self.entityList:
             entity.update()
+        if self.player.is_dead:
+            corpse = self.player.die()
+            self.entityList.add(corpse)
+            self.platforms.add(corpse)
 
     def handle_event(self, event):
         super().handle_event(event)
