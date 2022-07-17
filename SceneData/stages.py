b99e575be859cb897a2fb3ad4c60b26a7c3b1004
coinmoles <andrewpark421@gmail.com>
2022-07-17 16:15:33 +0900

    Rearranged stage order

--- SceneData/stages.py
+++ SceneData/stages.py
@@ -5,10 +5,11 @@ from SceneData.chapter1.stage1_1 import stage1_1
 from SceneData.chapter1.stage1_2 import stage1_2
 from SceneData.chapter1.stage1_3 import stage1_3
 from SceneData.chapter1.stage1_4 import stage1_4
-from SceneData.chapter1.stage1_6 import stage1_6
+from SceneData.chapter1.stage1_5 import stage1_5
 from SceneData.chapter1.stage1_7 import stage1_7
 from SceneData.chapter1.stage1_8 import stage1_8
-from SceneData.chapter1.stage1_9 import stage1_9
+from SceneData.chapter2.stage2_1 import stage2_1
+from SceneData.chapter2.stage2_2 import stage2_2
 
 
 stages = {
@@ -22,9 +23,9 @@ stages = {
         "stage2": stage1_2,
         "stage3": stage1_3,
         "stage4": stage1_4,
+        "stage5": stage1_5,
         "stage6": stage1_6,
         "stage7": stage1_7,
         "stage8": stage1_8,
-        "stage9": stage1_9,
     }
 }
