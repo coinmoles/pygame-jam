c37fc055665c1183709998c82f1f522b9a74bac5
coinmoles <andrewpark421@gmail.com>
2022-07-17 20:10:34 +0900

    Fixed jumpitem not flipping glitch

--- Entity/Corpse/JumpCorpse.py
+++ Entity/Corpse/JumpCorpse.py
@@ -1,8 +1,11 @@
+from typing import Tuple
 from pygame import Vector2
 from Entity.JumpPlatform import JumpPlatform
 from constants import FPS
 
+
 class JumpCorpse(JumpPlatform):
-    def __init__(self, pos: Vector2):
+    def __init__(self, pos: Vector2, flip: Tuple[bool, bool]):
         super().__init__(pos)
+        self.flip = flip
         self.set_sprites(["p2_duck", "p2_duck2"], FPS // 5)
