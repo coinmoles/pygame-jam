e66f3288b7ac3ddd0ec9814fbbe2d7d1e9702896
coinmoles <andrewpark421@gmail.com>
2022-07-17 22:48:13 +0900

    Adjusted save load

--- helper/save_load.py
+++ helper/save_load.py
@@ -5,7 +5,7 @@ from globals import GLOBALS
 
 
 def save_game(current_id: Tuple[int, int], file_id: int):
-    with open("saves/save{:02d}".format(file_id), "wb") as fw:
+    with open("saves/save{:02d}.pickle".format(file_id), "wb") as fw:
         pickle.dump({
             "current_id": current_id,
             "cleared_stages": GLOBALS.cleared_stages
@@ -13,10 +13,10 @@ def save_game(current_id: Tuple[int, int], file_id: int):
 
 
 def load_game(file_id: int):
-    if not os.path.isfile("saves/save{:02d}".format(file_id)):
+    if not os.path.isfile("saves/save{:02d}.pickle".format(file_id)):
         return None
 
-    with open("saves/save{:02d}".format(file_id), "rb") as fr:
+    with open("saves/save{:02d}.pickle".format(file_id), "rb") as fr:
         savedata = pickle.load(fr)
         GLOBALS.cleared_stages = savedata["cleared_stages"]
     
